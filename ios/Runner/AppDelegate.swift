c8f86e7d2b1cfb8fb2a50e9e1109a4a1ff98398c
naimulhassan2001 <developernaimul00@gmail.com>
2025-02-19 19:48:05 +0600

    improve code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,26 +1,30 @@
 import UIKit
 import Flutter
-import Firebase
+import GoogleMobileAds
+
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: {_, _ in })
-    } else {
-      let settings: UIUserNotificationSettings =
-      UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-      application.registerUserNotificationSettings(settings)
-    }
-    application.registerForRemoteNotifications()
-    FirebaseApp.configure()
+    // Register Flutter plugins
     GeneratedPluginRegistrant.register(with: self)
+    
+    // Initialize Google Mobile Ads SDK
+    GADMobileAds.sharedInstance().start(completionHandler: nil)
+
+    // For iOS 13+ using SceneDelegate, no need to manually set the window
+    if #available(iOS 13.0, *) {
+      // iOS 13+ uses SceneDelegate to manage the window
+    } else {
+      // For iOS versions before 13, manually configure the window
+      self.window = UIWindow(frame: UIScreen.main.bounds)
+      self.window?.rootViewController = FlutterViewController()
+      self.window?.makeKeyAndVisible()
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
